30bec47670b11ff8b728620493b72e04b38db774
skumar <skumar38017@outlook.com>
2025-04-25 19:52:45 +0000

    update redis config

--- redis_data/redis-entrypoint.sh
+++ redis_data/redis-entrypoint.sh
@@ -15,3 +15,4 @@ command:
   - --io-threads-do-reads yes  # Enable threads for reads too
 exec redis-server /etc/redis/redis.conf tagglabs@tagglabs:~Desktop/tg-inventory-management-system/redis_data
 redis-server redis_data/redis.conf
+
